828dfa40c6cac217291c3db0ca2a82fa94dd2f5d
serVerKim <scd1212@naver.com>
2024-06-27 20:46:28 +0900

    비디오 작성

--- src/components/KeyVisual.tsx
+++ src/components/KeyVisual.tsx
@@ -3,16 +3,20 @@ import styles from "./Keyvisual.module.css";
 const KeyVisual = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.img_wrapper}>
+      <section className={styles.img_wrapper}>
         <img className={styles.main_img} src="/img/kv_img_0825.png" />
         <img className={styles.mj_img} src="/img/kv_img_mj.png" />
         <img className={styles.tk_img} src="/img/kv_img_tk.png" />
-      </div>
-      <div className={styles.phase2_wrapper}>
+      </section>
+      <section className={styles.phase2_wrapper}>
         <img className={styles.phase2_img} src="/img/kv_img_phase2.png" />
         <img className={styles.heart_img} src="/img/kv_img_heart.png" />
         <img className={styles.wedding_img} src="/img/kv_img_wedding.png" />
-      </div>
+      </section>
+      <section className={styles.text}>
+        <div>8월 25일 낮 12시 30분</div>
+        <div>더화이트베일 1F 화이트베일 홀</div>
+      </section>
     </div>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import KeyVisual from "../components/KeyVisual";
 import LocationMap from "../components/LcoationMap";
+import Video from "../components/Video";
 
 const Main = () => {
   return (
     <div>
       <KeyVisual />
+      <Video />
       <LocationMap />
     </div>
   );
