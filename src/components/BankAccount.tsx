e663f537df0a2cf3ebe8e00ee21bb7f30f4b0998
serVerKim <scd1212@naver.com>
2024-06-30 22:11:32 +0900

    레이아웃 방식 수정

---
+++ src/components/BankAccount.tsx
@@ -0,0 +1,11 @@
+import styles from "./BankAccount.module.css";
+
+const BankAccount = () => {
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.title}>마음 전하실 곳</section>
+    </div>
+  );
+};
+
+export default BankAccount;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,6 +6,7 @@ import ImageSlider from "../components/ImageSlider";
 import Location from "../components/Location";
 import TransportList from "../components/TransportList";
 import PictureViewSection from "../components/PictureViewSection";
+import BankAccount from "../components/BankAccount";
 
 const Main = () => {
   return (
@@ -17,6 +18,7 @@ const Main = () => {
       <Location />
       <TransportList />
       <PictureViewSection />
+      <BankAccount />
     </div>
   );
 };
