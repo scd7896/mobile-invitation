4781645fe21e703e8fa25df6e7808c724069c361
serVerKim <scd1212@naver.com>
2024-07-01 21:44:02 +0900

    uxqa 적용

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,9 +3,14 @@ import styles from "./Video.module.css";
 const Video = () => {
   return (
     <section className={styles.video_wrapper}>
-      <video className={styles.video} autoPlay muted loop>
-        <source src="/img/main_video.mp4" type="video/mp4"></source>
-      </video>
+      <video
+        className={styles.video}
+        src="/img/main_video.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+      ></video>
       <div className={styles.deam} />
     </section>
   );
